5955f886a1e66cff9bc67a28b1d77dd061a907b7
Ági <raczagi07@gmail.com>
2023-06-05 22:53:33 +0200

    masodik feladat

--- src/Modulzaro.java
+++ src/Modulzaro.java
@@ -65,7 +65,14 @@ public class Modulzaro {
      * (3 pont)
      */
     public static int countEvenNumbers(int[] array) {
-        return -1;
+
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     /**
